01fcd01a2ca71083030f48cb33e7bd885bf6d386
Gerry <m@gerryeng.com>
2013-04-06 20:07:03 +0800

    ~ API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,15 @@
 class ApiController < ApplicationController
 
 	def sync
+		# Read Game state
 		sync_json = {
 			'players' => {
-				'status' => '123'
+				status:  '123',
+				who_s_turn: 123,
+				user_id: '123'
 			}
 		}
-		render :json => sync_json	
+		render :json => sync_json
 	end
 
 end
